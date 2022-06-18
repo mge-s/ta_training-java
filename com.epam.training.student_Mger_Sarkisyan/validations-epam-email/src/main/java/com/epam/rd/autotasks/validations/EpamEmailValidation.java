1cfd945b2786fc9ec17d288d045bfc5ab5d12829
Mger Sarkisyan <105071585+mge-s@users.noreply.github.com>
2022-06-18 18:06:28 +0400

    add validations-epam-email solution

---
+++ com.epam.training.student_Mger_Sarkisyan/validations-epam-email/src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -0,0 +1,22 @@
+package com.epam.rd.autotasks.validations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EpamEmailValidation {
+
+    public static boolean validateEpamEmail(String email) {
+        if (email == null)
+            return false;
+        Pattern pattern = Pattern.compile("((\\w+_)\\w+)@(epam.com)");
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+
+    }
+}
+
+
+
+
+
