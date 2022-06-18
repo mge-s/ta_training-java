6b3fad224f3ae1233cdeac0b66e4631eebf87f7d
Mger Sarkisyan <105071585+mge-s@users.noreply.github.com>
2022-06-18 17:22:19 +0400

    add pizza-split solution

---
+++ com.epam.training.student_Mger_Sarkisyan/pizza-split/src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -0,0 +1,28 @@
+package com.epam.rd.autotasks.pizzasplit;
+
+import java.util.Scanner;
+
+public class PizzaSplit {
+    public static void main(String[] args) {
+        //Write a program, reading number of people and number of pieces per pizza and then
+        //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+        Scanner scann = new Scanner(System.in);
+        int people = scann.nextInt();
+        int pieces = scann.nextInt();
+        int x = pieces;
+        int division;
+        while (pieces > 0 && people > 0) {
+            division = pieces % people;
+            if (division == 0)
+                break;
+            pieces += x;
+
+
+
+        }
+
+        System.out.println(pieces / x);
+
+
+    }
+}
